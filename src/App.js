74ff12e8576f6dce2e1f8e53bab022add32e1846
GuMarYae <tddanielsjr@gmail.com>
2021-12-29 17:52:22 -0600

    edited

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./Pages/Home/index.js";
 function App() {
   return (
     <div className="App">
-      <h6>IGod is good</h6>
+      <h6>God is good</h6>
       <h6>If you see this I love you, Shaina</h6>
       <Home/>
     </div>
